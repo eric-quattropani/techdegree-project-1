6181d594feec93aa98641983f8e2131e5ca1f3e3
eric-quattropani <eric.quattropani@gmail.com>
2021-04-06 14:08:19 +1000

    Created quotes array including 5 objects

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
  My first techdegree project
-Hello - test
--- js/script.js
+++ js/script.js
@@ -10,7 +10,30 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+var quotes = [
+  {
+    quote: 'Serenity Now!',
+    source: 'Frank Costanza',
+    citation: 'Seinfeld',
+    year: 1997
+  },
+  {
+    quote: 'Those who dare to fail miserably can achieve greatly',
+    source: 'John F. Kennedy'
+  },
+  {
+    quote: 'For a minute there, I lost myself',
+    source: 'David Brent'
+  },
+  {
+    quote: 'When people say to me: would you rather be thought of as a funny man or a great boss? My answer’s always the same, to me, they’re not mutually exclusive.',
+    source: 'David Brent' 
+  },
+  {
+    quote: 'You miss 100% of the shots you dont take.',
+    source: 'Wayne Gretzky' 
+  }
+];
 
 
 /***
